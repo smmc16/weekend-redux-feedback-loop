41dcb1c1402b23d923bfcaec79f14cab9aced515
Sarah McCartney <smmc16@gmail.com>
2024-04-30 20:26:59 -0500

    added ability to change responses on review page

--- src/components/ReviewPage/ReviewPage.jsx
+++ src/components/ReviewPage/ReviewPage.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { useState } from "react";
 import axios from 'axios';
 import { useHistory } from "react-router-dom";
 
@@ -6,10 +7,10 @@ function ReviewPage () {
     const history = useHistory();
 
     const inputArray = useSelector(store => store.feedback);
-    let q1 = inputArray[0];
-    let q2 = inputArray[1];
-    let q3 = inputArray[2];
-    let q4 = inputArray[3];
+    let [q1, setQ1] = useState(inputArray[0]);
+    let [q2, setQ2] = useState(inputArray[1]);
+    let [q3, setQ3] = useState(inputArray[2]);
+    let [q4, setQ4] = useState(inputArray[3]);
 
     const postArray = [q1, q2, q3, q4];
 
@@ -25,10 +26,10 @@ function ReviewPage () {
     return (
         <>
         <h3>Review Your Feedback</h3>
-        <p>Feeling:</p> <input value={q1} />
-        <p>Understanding: {q2}</p>
-        <p>Support: {q3}</p>
-        <p>Comments: {q4}</p>
+        <p>Feeling: {q1}</p> <input value={q1} onChange={(e) => setQ1(e.target.value)} />
+        <p>Understanding: {q2}</p> <input value={q2} onChange={(e) => setQ2(e.target.value)} />
+        <p>Support: {q3}</p> <input value={q3} onChange={(e) => setQ3(e.target.value)} />
+        <p>Comments: {q4}</p> <input value={q4} onChange={(e) => setQ4(e.target.value)} />
         <button data-testid="next" onClick={handleClick}>Submit</button>
         </>
     )
